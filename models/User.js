49c35bc78810e4a44fb83ede68218dbd199bacd3
yashsojitra97 <yashsojitra97@gmail.com>
2024-06-26 00:57:23 +0530

    secrets bug

--- models/User.js
+++ models/User.js
@@ -67,7 +67,6 @@ userSchema.statics.login = async function (email, password) {
     }
 
     const match = await bcrypt.compare(password, user.password)
-
     if(!match){
         throw Error("incorrect password")
     }
